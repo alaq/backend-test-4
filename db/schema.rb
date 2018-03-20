aec9d3183f22d4e967b2e2fbe32bc51c2ecb059d
Adrien Lacquemant <verblodung@gmail.com>
2018-03-19 20:30:32 -0400

    last change for db

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180319191902) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "calls", force: :cascade do |t|
     t.string "caller"
     t.string "status"
